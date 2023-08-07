32aff7b8912655d2030b6ea5dd8ecc7a47095678
Pebong2023 <ebongchris21@gmail.com>
2023-08-07 15:04:50 -0700

    create a new sentence

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted,interactive, object-oriented programminglanguage that combines remakable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[6]
+print(str)
