623b0d4b428788924542f7798ab0d1b82196d337
Pebong2023 <ebongchris21@gmail.com>
2023-08-08 10:00:22 -0700

    when i was having that alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for letter in range(97,123):
-    if chr(letter) not in ['q', 'e']:
-        print(chr(letter), end="")
+for k in range(97, 123):
+    if k not in [101, 113]:
+        print(chr(k), end='')
