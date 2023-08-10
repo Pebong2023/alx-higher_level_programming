eb20ba29f1804f15fc8746cdb3c425972212dd34
Pebong2023 <ebongchris21@gmail.com>
2023-08-10 15:22:31 -0700

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,12 +2,9 @@
 
 import sys
 
-def main():
+if __name__ == "__main__":
     """Print the sum of all arguments."""
     total = 0
     for i in range(1, len(sys.argv)):
         total += int(sys.argv[i])
     print("{}".format(total))
-
-if __name__ == "__main__":
-    main()
