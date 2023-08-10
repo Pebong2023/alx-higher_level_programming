b316796638a289f1ac5cb93d0a41ac4317cf9f9d
Pebong2023 <ebongchris21@gmail.com>
2023-08-10 15:26:22 -0700

    toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-def main():
-    """Print the sum, difference, product, and quotient of 10 and 5."""
-    from calculator_1 import add, sub, mul, div
+from calculator_1 import add, sub, mul, div
 
+if __name__ == "__main__":
+    """Print the sum, difference, product, and quotient of 10 and 5."""
     a = 10
     b = 5
 
@@ -11,6 +11,3 @@ def main():
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-
-if __name__ == "__main__":
-    main()
