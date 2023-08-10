4a5f3d0fb0b70ec6dec9b7c012657d6559128b67
Pebong2023 <ebongchris21@gmail.com>
2023-08-10 15:10:51 -0700

    toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-def print_operations(a, b):
-    """Prints the results of various operations between two numbers."""
-    sum_result = add(a, b)
-    difference_result = sub(a, b)
-    product_result = mul(a, b)
-    quotient_result = div(a, b)
-
-    print(f"{a} + {b} = {sum_result}")
-    print(f"{a} - {b} = {difference_result}")
-    print(f"{a} * {b} = {product_result}")
-    print(f"{a} / {b} = {quotient_result}")
 
 def main():
-    """Calculate and print the results of operations between 10 and 5."""
+    """Print the sum, difference, product, and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
-    print_operations(a, b)
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
 if __name__ == "__main__":
     main()
