3187d00e99f3824e2e9c0996772b86899eac5ce4
Pebong2023 <ebongchris21@gmail.com>
2023-08-11 19:11:29 -0700

    fast alphabet

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 import string
-print(string.ascii_uppercase)
+
+def main():
+    uppercase_letters = string.ascii_uppercase
+    print(uppercase_letters)
+
+if __name__ == "__main__":
+    main()
