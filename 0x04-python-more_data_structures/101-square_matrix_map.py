7939d3f20ee73b884d9885af29a7735586a8402a
Pebong2023 <ebongchris21@gmail.com>
2023-08-16 18:48:30 -0700

    squared by using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -3,5 +3,4 @@ def square_matrix_map(matrix=[]):
     """
     Calculate the square of each element in a matrix using map and lambda functions.
     """
-    squared_matrix = (list(map(lambda row: list(map(lambda y: y ** 2, row)), matrix)))
-    return( squared_matrix)
+    return (list(map(lambda row: list(map(lambda y: y ** 2, row)), matrix)))
