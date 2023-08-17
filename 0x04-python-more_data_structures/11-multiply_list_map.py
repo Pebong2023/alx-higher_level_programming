9dca59c6312a3bbd113d9cf8b38886a54858273a
Pebong2023 <ebongchris21@gmail.com>
2023-08-16 18:28:43 -0700

    multiply using map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def multiply_list_elements(my_list=[], factor=1):
-    """
-    Multiply elements in a list by a specified factor using map and a lambda function.
-    """
-    multiplied_list = list(map(lambda x: x * factor, my_list))
-    return (multiplied_list)
+def multiply_list_map(my_list=[], number=0):
+    return (list(map((lambda i: i * number), my_list)))
