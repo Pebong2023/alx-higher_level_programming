66c6777a7adb838970e548a24acfa00bb5ba8c9f
Pebong2023 <ebongchris21@gmail.com>
2023-09-11 14:56:17 -0700

    exact same object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-"""Defines an inherited list class MyList."""
 
+"""
+Defines a class-checking function.
+"""
 
-class MyList(list):
-    """Implements sorted printing for the built-in list class."""
+def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of a given class.
 
-    def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self))
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    return type(obj) == a_class
+    return True
+    return False
