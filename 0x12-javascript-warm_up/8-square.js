837dc2704947521d4f20b29c91e48ea7adec3f5e
root <ebongchris21@gmail.com>
2023-11-13 12:52:52 -0800

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+const argv = require('process').argv;
+const num = Number(argv[2]);
+
+if (isNaN(num)) {
   console.log('Missing size');
 } else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
-    console.log('X'.repeat(x));
-    i++;
+  let square = 'X';
+  for (let i = 1; i < num; i++) {
+    square = square + 'X';
+  }
+  for (let j = 0; j < num; j++) {
+    console.log(square);
   }
 }
