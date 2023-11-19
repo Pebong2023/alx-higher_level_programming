a74997114143bba5c47a6d00ed2c406517d29ea7
root <ebongchris21@gmail.com>
2023-11-19 03:06:42 -0800

    Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+"""Contains a script that lists all State objects from the database
 """
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
 
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    eng = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2],
+                                                      sys.argv[3])
+    engine = create_engine(eng)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        session.delete(instance)
+
+    states = session.query(State).filter(State.name.ilike("%a%")).delete(
+                synchronize_session='fetch')
     session.commit()
+
+    session.close()
